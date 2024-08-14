782560b48262461b3709c2ce10fcdb4ee7f10827
ariel3dprintinguy <queenjelly0@gmail.com>
2024-08-14 09:44:51 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var exec = require('child_process').exec;
+let express = require('express')
+const multer = require("multer");
+const fileUpload = require("express-fileupload")
+var bodyParser = require('body-parser')
+const fs = require("fs")
+const app = express()
+//app.use(fileUpload());
+app.use(
+    bodyParser.raw({limit: "50mb", type: ['model/*']})
+);
+//const upload = multer({ dest: "uploads/" });
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.send("hello world")
+})
+
+app.post("/3d", (req, res) => {
+    const type = req.get('Content-Type')
+    const ext = type.split("/")[1]
+    console.log("ext", ext);
+//	console.log(req)
+    const b = req.body
+    const fileName = "file_" + new Date().toISOString() + "." + ext
+    fs.writeFile(fileName, b, "binary", function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("The file was saved!");
+            const outFile = "out_" + new Date().toISOString() + ".3mf"
+            exec(`./prusaslicer/prusa-slicer --slice 0 --debug 2 --export-3mf ${outFile} ${fileName}`, (err, stdout, stderr) => {
+                if (err) {
+                    // node couldn't execute the command
+                    return;
+                }
+
+                // the *entire* stdout and stderr (buffered)
+                console.log(`stdout: ${stdout}`);
+                console.log(`stderr: ${stderr}`);
+                // Completed:
+                res.sendFile(outFile)
+
+            });
+        }
+    });
+})
+
+
+app.listen(28508, () => {
+    console.log("listening on 28508")
+})
+///
+//console.log("h")
+
